556a98c7e2f21fdd995ca8701eb7037177c8bf7e
David Aguilar <gd.aguilarc@gmail.com>
2019-03-02 19:58:55 -0600

    Project scripts

---
+++ reduce.js
@@ -0,0 +1,10 @@
+/**
+ * This script is in charge of performing the minimization
+ * of the AFD resulting from the transformation of the AFN
+ *
+ * @author      David Aguilar
+ * @author      Andrea Becerra
+ * @author      Saul Neri
+ * @version     1.0.0
+ * @since       1.0.0
+ */
---
+++ transformation.js
@@ -0,0 +1,9 @@
+/**
+ * This script is in charge of performing the transformation from a Lambda AFN to a AFD
+ *
+ * @author      David Aguilar
+ * @author      Andrea Becerra
+ * @author      Saul Neri
+ * @version     1.0.0
+ * @since       1.0.0
+ */
