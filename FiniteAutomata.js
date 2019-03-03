dd177e55fa699ad8ebbe074764834b98bbcbdf18
David Aguilar <gd.aguilarc@gmail.com>
2019-03-02 20:24:29 -0600

    Finite Automata :trollface:

---
+++ FiniteAutomata.js
@@ -0,0 +1,9 @@
+class FiniteAutoma {
+  constructor(states, alphabet, initial, final, transitions) {
+    this.states = states;
+    this.alphabet = alphabet;
+    this.initial = initial;
+    this.final = final;
+    this.transitions = transitions;
+  }
+}
