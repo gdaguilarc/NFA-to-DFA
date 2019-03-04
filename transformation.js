d78445a0589f4a78a51093f4ad4b86fdd24a2cbb
AndreaBecerra <andii1wiki@gmail.com>
2019-03-04 15:17:32 -0600

    closureOfClosure

--- transformation.js
+++ transformation.js
@@ -7,3 +7,26 @@
  * @version     1.0.0
  * @since       1.0.0
  */
+require('./FiniteAutomata');
+
+function transformation(automata) {
+  Q = new FiniteAutoma();
+  Q.addInitial(automata.initial);
+
+  newState = closureOfClosure(automata);
+}
+function closureOfClosure(automata, state, letter) {
+  var arr = automata.lambdaLock(state);
+  arr.forEach(element => {
+    //buscar los estados finales a donde llegan con letter
+    var transitions = automata.transitions[letter].filter(elem => {
+      return elem.initial === state;
+    });
+
+    return transitions
+      .forEach(elem => {
+        return automata.lambdaLock(elem);
+      })
+      .join(',');
+  });
+}
