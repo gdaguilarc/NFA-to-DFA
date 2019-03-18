96868a7008285caedae389cceee74324f371a687
David Aguilar <gd.aguilarc@gmail.com>
2019-03-18 11:17:15 -0600

    Completed algorithm, needs to be tested. Also needs to change de error state

--- src/transformation.js
+++ src/transformation.js
@@ -30,6 +30,32 @@ function tableOfClosures(automata) {
   return tableClosures;
 }
 
+function tableTCreation(closure, letter, automata, tableClosures) {
+  let temp = [];
+  const transitionsT = getTransitions(closure, letter, automata);
+
+  if (transitionsT !== '') {
+    // Get the lambda lock of the new transitions
+    transitionsT.split(',').forEach(elem => {
+      temp = temp.concat(tableClosures[elem].closure);
+    });
+
+    // Only the unique
+    temp = temp.filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+
+    // Sort
+    temp.sort((a, b) => {
+      return a > b;
+    });
+  } else {
+    temp = ['0'];
+  }
+
+  return temp;
+}
+
 function getTransitions(array, letter, automata) {
   const letterArray = [];
   const result = [];
@@ -66,7 +92,7 @@ function Transformation(automata) {
 
   // Add initial
   // TODO: Needs to be an array
-  result.addState(tableClosures[automata.initial]);
+  result.addState(tableClosures[automata.initial].closure.join(','));
   result.addInitial(result.states[0]);
 
   // Adds alphabet
@@ -85,43 +111,46 @@ function Transformation(automata) {
   automata.states.forEach(st => {
     automata.alphabet.forEach(letter => {
       // Closure of the current state
-      let temp = [];
-      const transitionsT = getTransitions(tableClosures[st].closure, letter, automata);
-
-      if (transitionsT !== '') {
-        // Get the lambda lock of the new transitions
-        transitionsT.split(',').forEach(elem => {
-          temp = temp.concat(tableClosures[elem].closure);
-        });
-
-        // Only the unique
-        temp = temp.filter((value, index, self) => {
-          return self.indexOf(value) === index;
-        });
-
-        // Sort
-        temp.sort((a, b) => {
-          return a > b;
-        });
-      } else {
-        temp = ['0'];
-      }
 
       if (!tableT[st]) {
         tableT[st] = [];
       }
-
-      tableT[st].push(temp);
+      tableT[st].push(tableTCreation(tableClosures[st].closure, letter, automata, tableClosures));
     });
   });
 
-  return tableT;
+  for (let i = 0; i < result.states.length; i += 1) {
+    result.alphabet.forEach(letter => {
+      const letterIndex = result.alphabet.indexOf(letter);
+      let y = [];
+      result.states[i].split(',').forEach(elem => {
+        if (tableT[elem] && tableT[elem][letterIndex].join(',') !== '0') {
+          y.push(tableT[elem][letterIndex].join(','));
+        }
+      });
+
+      y = y.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+      });
+
+      y = y.join(',');
+
+      if (result.states.includes(y)) {
+        result.addTransition(result.states[i], y, letter);
+      } else {
+        result.addState(y);
+        result.addTransition(result.states[i], y, letter);
+      }
+    });
+  }
+
+  return result;
 }
 
 let a = new AFN();
-a.addLetter('a');
-a.addLetter('b');
 a.addLetter('c');
+a.addLetter('b');
+a.addLetter('a');
 a.addState('q0');
 a.addState('q1');
 a.addState('q2');
@@ -134,6 +163,6 @@ a.addTransition('q1', 'q1', 'b');
 a.addTransition('q2', 'q2', 'c');
 a.addTransition('q2', 'q1');
 
-console.log('Tabla T \n', Transformation(a));
+console.log('RESULT \n', Transformation(a).transitions);
 
 module.exports = Transformation;
